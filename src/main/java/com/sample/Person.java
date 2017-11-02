e0da0628a15042ce6a90f0cbc48eebfb2212982f
KonishiYuji <konishiyuji@KonishiYuji-no-MacBook-Air.local>
2017-11-02 20:08:12 +0900

    init

---
+++ src/main/java/com/sample/Person.java
@@ -0,0 +1,45 @@
+package com.sample;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author konishiyuji
+ */
+@Entity
+public class Person implements Serializable {
+    @Id
+    private int id;
+    private String name;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * getterです
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
