4d84d078f3b3d8fe359107abae47dc6d275826c9
Yuji Konishi <yuji.k64613@gmail.com>
2019-03-31 13:19:27 +0900

    simpel version app

--- src/main/java/com/sample/Main.java
+++ src/main/java/com/sample/Main.java
@@ -1,9 +1,5 @@
 package com.sample;
 
-import java.io.ByteArrayInputStream;
-
-import javax.ejb.EJB;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,36 +13,11 @@ import javax.ws.rs.core.Response.ResponseBuilder;
  */
 @Path("main")
 public class Main {
-	@Inject
-	private SessionDTO dto;
-	@EJB
-	private PersonDB db;
-
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("foo")
 	public String foo(@QueryParam("id") int id) {
-		Integer n = (Integer) dto.get("COUNT");
-		if (n == null) {
-			n = 0;
-		}
-		n++;
-		dto.put("COUNT", n);
-
-		Person p = db.find(id);
-		return (p != null) ? "result=" + id + ":" + p.getName() + ":" + n : "ERROR!!";
-	}
-
-	@GET
-	@Produces("text/plain")
-	@Path("bar")
-	public Response bar(@QueryParam("id") int id) {
-		byte[] b = new byte[] { (byte) 0x97, (byte) 0x98, (byte) 0x99, (byte) 0x100 };
-
-		ByteArrayInputStream bos = new ByteArrayInputStream(b);
-		ResponseBuilder response = Response.ok(bos);
-		String headerVal = "attachment; filename=hogehoge.txt";
-		response.header("Content-Disposition", headerVal);
-		return response.build();
+		id++;
+		return "result=" + id;
 	}
 }
